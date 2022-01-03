15e5617e325a972e5f799aa58a1b01e97883b3bc
unknown <hochard@et.esiea.fr>
2022-01-03 20:10:20 +0100

    Part 3: Serveur & Call Handler <done>

--- src/main/java/fr/lernejo/navy_battle/Server.java
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -1,12 +1,19 @@
 package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpServer;
+import fr.lernejo.navy_battle.handler.CallHandler;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class Server {
-    HttpServer httpserver;
-    Server(int port) throws IOException {
-        httpserver = HttpServer.create(new InetSocketAddress(port), 0);
+    public void startServeur() throws IOException {
+        HttpServer httpServer;
+            httpServer = HttpServer.create(new InetSocketAddress(9867), 0);
+            httpServer.setExecutor(newFixedThreadPool(1));
+            httpServer.createContext("/ping", new CallHandler());
     }
 }
--- src/main/java/fr/lernejo/navy_battle/handler/CallHandler.java
+++ src/main/java/fr/lernejo/navy_battle/handler/CallHandler.java
@@ -1,15 +1,16 @@
 package fr.lernejo.navy_battle.handler;
-import java.io.IOException;
-import java.io.OutputStream;
+
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class CallHandler implements HttpHandler{
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		 String body = "Navy battle server ready to use.";
-		 exchange.sendResponseHeaders(150, body.length());
-
+		 String body = "OK";
+		 exchange.sendResponseHeaders(200, body.length());
 		 try (OutputStream os = exchange.getResponseBody()) {
 		     os.write(body.getBytes());
 		 }
